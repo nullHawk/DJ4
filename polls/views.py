1a8f0c6e5fa2bc835effd32848eb2c7baa59fa70
Suryansh Shakya <suryanshsinghshakya1@gmail.com>
2023-10-20 22:52:21 +0530

    .

--- polls/views.py
+++ polls/views.py
@@ -3,13 +3,14 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    http_file ='''
+    #return render(request,'polls/index.html')
+    http_file='''
     <html>
         <head>
             <title>Eureka!!!</title>
         </head>
         <body>
-            <h1 align='center'> Greetings!</h1>
+            <h1 align='center'> Greetings !</h1>
             <p align='center'> by Suryansh Shakya </p>
         </body>
     </html>
