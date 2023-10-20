ba0e170c611d7a39762c911edb0eb3ecca423a7a
Suryansh Shakya <suryanshsinghshakya1@gmail.com>
2023-10-21 02:41:06 +0530

    editing the templates

--- polls/urls.py
+++ polls/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns=[
     path('', views.index, name='index'),
+    path('funcky',views.funcky),
+    path('danger/<int:guess>', views.danger),
+    path('google', views.google),
+    path('game/<slug:guess>', views.GameView.as_view())
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,18 +1,36 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.utils.html import escape
+from django.views import View
 
 # Create your views here.
 def index(request):
-    #return render(request,'polls/index.html')
-    http_file='''
+    response='''
     <html>
         <head>
             <title>Eureka!!!</title>
         </head>
         <body>
-            <h1 align='center'> Greetings Papa!</h1>
+            <h1 align='center'> Greetings!</h1>
             <p align='center'> by Suryansh Shakya </p>
         </body>
     </html>
     '''
-    return HttpResponse(http_file)
+    return HttpResponse(response)
+
+def funcky(request):
+    response=''' Hii '''
+    return HttpResponse(response)
+
+def google(request):
+    return HttpResponseRedirect("www.google.co.in")
+
+def danger(request,guess):
+    response = ''' Your guess was ''' + escape(guess)
+    return HttpResponse(response)
+
+class GameView(View):
+    def get(self, request, guess):
+        x = {'guess':int(guess)}
+        return render(request, 'polls/cond.html', x)
