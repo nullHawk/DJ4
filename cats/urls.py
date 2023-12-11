1679a6a47b2d1de23e250064cf7db254c63c67b2
Suryansh Shakya <suryanshsinghshakya1@gmail.com>
2023-12-11 10:42:03 +0530

    fixed url issue

--- cats/urls.py
+++ cats/urls.py
@@ -5,5 +5,4 @@ urlpatterns = [
     path('', views.CatList.as_view(), name='all'),
     path('main/create/', views.CatCreate.as_view(), name='cat_create'),
     path('main/<int:pk>/update/', views.CatUpdate.as_view(), name='cat_update'),
-    ...
 ]
