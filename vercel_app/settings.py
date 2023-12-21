12a9e008f48f0ae3d7f6c35796a3428639d83f90
Suryansh Shakya <suryanshsinghshakya1@gmail.com>
2023-12-21 10:47:03 +0530

    fixed crispy-forms bug

--- vercel_app/settings.py
+++ vercel_app/settings.py
@@ -50,8 +50,15 @@ INSTALLED_APPS+=[
     'hello',
     'autos',
     'ads',
+    'social_django',
     'cats.apps.CatsConfig',
+    "crispy_forms",
+    "crispy_bootstrap5",
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -134,6 +141,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
