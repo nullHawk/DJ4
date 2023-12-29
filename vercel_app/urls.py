5870b55a661ccda0665586ab971672f3006d3436
Suryansh Shakya <suryanshsinghshakya1@gmail.com>
2023-12-29 12:51:19 +0530

    ads 3.0

--- vercel_app/urls.py
+++ vercel_app/urls.py
@@ -1,20 +1,9 @@
-"""vercel_app URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+import os
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.conf import settings
+from django.views.static import serve
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,3 +17,23 @@ urlpatterns = [
     path('cats/', include('cats.urls')),
     path('secret_santa/', include('secret_santa.urls')),
 ]
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+urlpatterns += [
+    re_path(r'^site/(?P<path>.*)$', serve, {'document_root': os.path.join(BASE_DIR, 'site'), 'show_indexes': True}, name='site_path'),
+]
+
+# Serve the favicon - Keep for later
+urlpatterns += [
+    path('favicon.ico', serve, {'path': 'favicon.ico', 'document_root': os.path.join(BASE_DIR, 'home/static')}),
+]
+
+# Switch to social login if it is configured - Keep for later
+try:
+    from . import github_settings
+    social_login = 'registration/login_social.html'
+    urlpatterns.insert(0, path('accounts/login/', auth_views.LoginView.as_view(template_name=social_login)))
+    print('Using', social_login, 'as the login template')
+except ImportError:
+    print('Using registration/login.html as the login template')
